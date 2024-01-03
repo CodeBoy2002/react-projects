c17d5458f0d9df2b698aef361ea850e37864cbe8
Sumit <marvelsumit2002@gmail.com>
2024-01-03 19:50:47 +0530

    cart commit

---
+++ cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: 'https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s7--.jpg',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img: 'https://images.pexels.com/photos/1482061/pexels-photo-1482061.jpeg?auto=compress&cs=tinysrgb&w=600',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: 'https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg',
+    amount: 1,
+  },
+];
