5191e5e35e80271da5369ef611c07afeed72f651
Sumit <marvelsumit2002@gmail.com>
2024-01-04 17:52:09 +0530

    cocktail unfinished

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur massa
+        risus, volutpat ac nisi a, efficitur vulputate felis. Aliquam fringilla
+        nibh magna, et suscipit augue ornare id. Quisque sapien mauris,
+        ullamcorper ut est at, convallis rutrum nibh. Quisque vulputate leo
+        tortor, pretium cursus orci ullamcorper iaculis. Nulla facilisi. Donec
+        faucibus sodales mauris ac mattis. Sed placerat augue at enim tristique,
+        ut faucibus nisi aliquet. Vestibulum ornare sollicitudin dui finibus
+        scelerisque. Nam consectetur quam elit, vitae pulvinar eros placerat et.
+        Duis tincidunt convallis feugiat. In aliquam ex a lectus sollicitudin
+        facilisis. Praesent et ipsum elit. Aliquam porttitor egestas metus, ac
+        aliquet neque vehicula vulputate. Nunc nisl arcu, consequat in aliquam
+        ultricies, pellentesque id urna.
+      </p>
+    </section>
+  );
+};
+
+export default About;
